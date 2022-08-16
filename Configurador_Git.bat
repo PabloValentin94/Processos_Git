e4a326af74b61005f7e94bcb23f23a0ad98a451e
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-08-16 15:53:41 -0300

    First Commit

---
+++ Configurador_Git.bat
@@ -0,0 +1,11 @@
+C:
+
+cd\
+
+git config --global user.name "PABLO VALENTIN"
+
+git config --global user.email "pablo.valentin@etec.sp.gov.br"
+
+git config --list
+
+pause
---
+++ Instalador_Git.bat
@@ -0,0 +1,13 @@
+E:
+
+copy E:\Processos_Git\Git-2.36.1-64-bit.exe C:\Users\Usuario\Desktop
+
+C:
+
+cd\
+
+cd C:\Users\Usuario\Desktop
+
+Git-2.36.1-64-bit.exe
+
+pause
